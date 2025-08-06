58f2001c92f2a31a24312f72cc5bc3f57fcb648c
Freedom <freewhynane62@gmail.com>
2025-08-06 21:01:56 +0000

    add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ['Noto Sans Display Variable', 'sans-serif'],
+        secondary: ['Inter Variable', 'sans-serif'],
+      },
+      fontSize: {
+        min: '12pt',
+        max: '96pt',
+        body: '14pt',
+        subheading: '18pt',
+        heading: '22pt',
+      },
+      lineHeight: {
+        min: '1',
+        max: '1.5',
+      },
+    },
   },
   plugins: [],
 };
-
