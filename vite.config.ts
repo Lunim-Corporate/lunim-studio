bd37a083f49793b627e3bf6e9a044ff23543d2a3
Freedom <freewhynane62@gmail.com>
2025-08-07 19:18:11 +0000

    add sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemap  from 'vite-plugin-sitemap';
 
-// https://vitejs.dev/config/
+const routes = [
+  '/', '/about', '/contact', '/projects', '/services',
+  '/blog', '/team', '/privacy-policy', '/terms-of-service',
+  '/cookie-policy', '/legal-notice', '/imprint', '/faq'
+];
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://studio.lunim.io',
+      dynamicRoutes: routes,
+      changefreq: 'weekly',
+      priority: 0.8,
+      lastmod: new Date(),
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
